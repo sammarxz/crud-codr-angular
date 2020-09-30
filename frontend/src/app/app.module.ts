c144bb103b3e29dd32a83ba49e1bb6299b81c0c3
Sam Marxz <sammarxzz@gmail.com>
2020-09-30 16:49:57 -0300

    Add Header and Footer Components.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Components
+import { HeaderComponent } from './component/template/header/header.component';
+
+// Material UI
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './component/template/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
